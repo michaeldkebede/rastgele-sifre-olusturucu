0d08f7c41e1d34a09dce3c81c409de2fbc38b1a6
Michael Dereje K <Michaeldkebede@gmail.com>
2023-06-19 20:34:00 +0300

    Update main.py
    ikinci versiyon

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import discord
 import random
+import webbrowser as web
 
-yazi = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# istemci (client) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+intents=discord.Intents.all()
+intents = discord.Intents()
+intents.members = True
 
-sifre_uzunlugu = int(input("Şifre uzunluğu ne kadar olsun: "))
+@client.event
+async def on_ready():
+    print(f'Hi boyle girdim {client.user}')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    elif message.content.startswith('$hello'):
+        #web.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        await message.channel.send("AHAHAHAHAHAHAHAHAHA!")
 
-for _ in range(sifre_uzunlugu):
-    print(random.choice(yazi), end="")
+@client.event
+async def on_member_join(member):
+    await member.send('Welcome to the server!')
 
+client.run("MTExNzg1NzQ2NDExMzcwOTIxNg.GhBGTN.fw9DD9zs6luVgnmlEQZYtM20TSqSNh2aBG_45o")
